1b61d44c7a5d5570703d69fb516957bfbd46fd87
dbrrlwjd <drw5522@gmail.com>
2023-08-29 17:28:13 +0900

    게시판

--- resources/js/myJs.js
+++ resources/js/myJs.js
@@ -5,3 +5,20 @@ function f_rgbColor(){
     + Math.floor(Math.random()*256)
     + ")";
 }
+
+// 전달되어 온 데이터에 대해 key값의 value를 리턴
+function getParameter(p_key){
+    let v_url = location.href;
+    // 주소 내에 ?가 존재하는지 확인
+    if(v_url.indexOf('?') != -1){
+        let v_queryString = v_url.split('?')[1];
+
+        let v_prams = v_queryString.split('&');
+
+        for(let i=0;i<v_prams.length;i++){
+            if(v_prams[i].split('=')[0] == p_key){
+                return decodeURIComponent(v_prams[i].split('=')[1]);
+            }
+        }
+    }
+}
