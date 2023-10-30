aa6b4e319b731c9df8029d253384799d7fd1aef5
dbrrlwjd <drw5522@gmail.com>
2023-10-30 18:11:54 +0900

    ppt만 남았다

--- ch04_data/GiJung/yugioh.js
+++ ch04_data/GiJung/yugioh.js
@@ -6,9 +6,19 @@ const v_info = document.querySelector(".info");
 const v_loading = document.querySelector("#loading");
 let decks = [], tmpDeck = { 1: [], 2: [], 3: [] };
 let shuffleDeck = [];
+let decks2;
 
 v_loading.style.display = 'block';
 let v_ajax = new XMLHttpRequest();
+let v_ajax2 = new XMLHttpRequest();
+v_ajax2.open("GET", "./decks.json");
+v_ajax2.onload = function(){
+    data = JSON.parse(v_ajax2.response);
+    decks2 = data['data'];
+    console.log(decks2);
+}
+v_ajax2.send();
+
 v_ajax.open("GET", "https://db.ygoprodeck.com/api/v7/cardinfo.php");
 v_ajax.onload = function () {
     cardDB = JSON.parse(v_ajax.response);
@@ -58,11 +68,13 @@ v_ajax.onload = function () {
     console.log(cardData);
     console.log(banlist_info);
     console.log(card_sets);
+    //decks = localStorage.getItem("decks");
     v_loading.style.display = 'none';
 }
 v_ajax.send();
 
 function f_search() {
+    v_loading.style.display = 'block';
     v_result.innerHTML = '';
     for (let i = 0; i < cardData.length; i++) {
         if (cardData[i]['name'].toLowerCase().indexOf(v_input.value.toLowerCase()) != -1 &&
@@ -71,6 +83,7 @@ function f_search() {
             f_slot(cardData[i]['card_images'][0]['image_url'], cardData[i]['id']);
         }
     }
+    v_loading.style.display = 'none';
 }
 
 function f_slot(p_src, p_id) {
@@ -143,39 +156,83 @@ function f_appendCard(p_event, n) {
 
 function f_info(p_id) {
     v_info.innerHTML = '';
-    let v_card = {};
+    let v_cardData = {};
     for (let i = 0; i < cardData.length; i++) {
         if (cardData[i]['id'] == p_id) {
-            v_card = cardData[i];
+            v_cardData = cardData[i];
         }
     }
-    console.log(v_card);
+    console.log(v_cardData);
+
     let v_img = document.createElement('img');
-    v_img.src = v_card['card_images'][0]['image_url_cropped'];
+    v_img.src = v_cardData['card_images'][0]['image_url_cropped'];
     v_img.style.width = '100%';
     v_info.appendChild(v_img);
 
     let v_name = document.createElement('p');
-    v_name.innerHTML = v_card['name'];
+    v_name.innerHTML = v_cardData['name'];
     v_info.appendChild(v_name);
 
     let v_desc = document.createElement('p');
-    v_desc.innerHTML = v_card['desc'];
+    v_desc.innerHTML = v_cardData['desc'];
     v_info.appendChild(v_desc);
 
-    let v_a = document.createElement('a');
-    v_a.innerHTML = "이 카드를 사용한 덱 보기";
-    v_a.href = ""
-    v_info.appendChild(v_a);
+    let v_btn = document.createElement('button');
+    v_btn.innerHTML = "이 카드를 사용한 덱 보기";
+    v_btn.addEventListener("click", () => {
+        v_result.innerHTML = '';
+        for (let i = 0; i < decks2.length; i++) {
+            for (let j = 0; j < decks2[i]['deckCetegory'].length; j++) {
+                if(v_cardData['name'].indexOf(decks2[i]["deckCetegory"][j]) != -1){
+                    let v_div = document.createElement('div');
+                    v_div.innerHTML = decks2[i]["deckName"];
+                    v_div.addEventListener("click", ()=>{
+                        f_reset();
+                        v_deck.children[0].children[0].value = decks2[i]['deckName'];
+                        v_deck.children[0].children[1].value = decks2[i]['deckCetegory'];
+                        for (let d = 1; d <= 3; d++) {
+                            for (let n= 0; n < decks2[i]["deck"][d].length; n++) {
+                                let v_card = document.createElement('img');
+                                v_card.src = "./resources/552E46D7356720000F.jpeg";
+                                for (let k = 0; k < cardData.length; k++) {
+                                    if (decks2[i]["deck"][d][n] == cardData[k]['name']) {
+                                        v_card.src = cardData[k]['card_images'][0]['image_url'];
+                                        v_card.id = cardData[k]['id'];
+                                        tmpDeck[d].push(cardData[k]['id']);
+                                        break;
+                                    }
+                                    // v_card.id = '';
+                                    // tmpDeck[d].push('');
+                                }
+                                v_card.width = 50;
+                                v_card.addEventListener('mousedown', () => {
+                                    if (event.button == 0) {
+                                        f_info(event.target.id);
+                                    }
+                                    if (event.button == 2) {
+                                        v_deck.children[d].removeChild(event.target);
+                                        tmpDeck[d].slice(tmpDeck[n].indexOf(event.target.id), 1);
+                                    }
+                                });
+                                v_deck.children[d].appendChild(v_card);
+                            }
+                        }
+                    });
+                    v_result.appendChild(v_div);
+                }
+            }            
+        }        
+    });
+    v_info.appendChild(v_btn);
 
-    if (v_card['card_sets']) {
+    if (v_cardData['card_sets']) {
         let v_ul = document.createElement("ul");
-        for (let j = 0; j < v_card['card_sets'].length; j++) {
+        for (let j = 0; j < v_cardData['card_sets'].length; j++) {
             let str = '';
             for (let i = 0; i < cardData.length; i++) {
                 if (cardData[i]['card_sets']) {
                     if (cardData[i]['card_sets'][j]) {
-                        if (v_card['card_sets'][j]['set_name'] == cardData[i]['card_sets'][j]['set_name']) {
+                        if (v_cardData['card_sets'][j]['set_name'] == cardData[i]['card_sets'][j]['set_name']) {
                             str += cardData[i]['card_images'][0]['image_url'] + " ";
                         }
                     }
@@ -184,8 +241,8 @@ function f_info(p_id) {
             }
             let v_li = document.createElement('li');
             let v_sets = document.createElement('a');
-            v_sets.innerHTML = v_card['card_sets'][j]['set_name'];
-            v_sets.href = "./store.html?set_name=" + v_card['card_sets'][j]['set_name'] +
+            v_sets.innerHTML = v_cardData['card_sets'][j]['set_name'];
+            v_sets.href = "./store.html?set_name=" + v_cardData['card_sets'][j]['set_name'] +
                 "&card_set=" + str;
             v_li.appendChild(v_sets);
             v_ul.appendChild(v_li);
@@ -250,6 +307,8 @@ function f_draw() {
 }
 
 function f_reset() {
+    v_deck.children[0].children[0].value = '';
+    v_deck.children[0].children[1].value = '';
     v_deck.children[1].innerHTML = "<p>메인 덱</p>";
     v_deck.children[2].innerHTML = "<p>엑스트라 덱</p>";
     v_deck.children[3].innerHTML = "<p>사이드 덱</p>";
@@ -262,7 +321,7 @@ function f_save() {
 
     for (let k = 0; k < decks.length; k++) {
         if (decks[k]["deckName"] == deckName) {
-            return;
+            decks.slice(k,1);
         }
     }
 
@@ -279,7 +338,8 @@ function f_save() {
             for (let d = 0; d < decks.length; d++) {
                 if (decks[d]["deckName"] == deckName) {
                     f_reset();
-
+                    v_deck.children[0].children[0].value = decks[d]['deckName'];
+                    v_deck.children[0].children[1].value = decks[d]['deckCategory'];
                     for (let i = 1; i <= 3; i++) {
                         for (let j = 0; j < decks[d]["deck"][i].length; j++) {
                             for (let k = 0; k < cardData.length; k++) {
@@ -313,7 +373,7 @@ function f_save() {
         v_deck.children[9].appendChild(v_div);
         v_deck.children[9].scrollTop = v_deck.children[9].scrollHeight;
         decks.push({ "deckId": decks.length > 0 ? decks[decks.length]["deckId"]+1 : 1,
-            "deckName": deckName, "deckCetegory": v_deck.children[0].children[1].value, "deck": tmpDeck })
+            "deckName": deckName, "deckCategory": v_deck.children[0].children[1].value, "deck": tmpDeck })
         //localStorage.setItem("decks", decks);
     }
 }
